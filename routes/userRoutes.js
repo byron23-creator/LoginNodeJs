4436b3fac2f5bd664df0af2da0c83e092a9b5cea
Byron Fernando Cardona Sánchez <68670367+byron23-creator@users.noreply.github.com>
2025-06-10 21:53:30 -0600

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUserProfile, protectedRegisterUser } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/profile', protect, getUserProfile);
-router.post('/protected-register', protect, protectedRegisterUser);
-
-module.exports = router;
