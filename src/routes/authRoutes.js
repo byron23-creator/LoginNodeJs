37b2873789bcf7e33b84829a1f9e4cbe164f1dbc
Byron Fernando Cardona Sánchez <68670367+byron23-creator@users.noreply.github.com>
2025-06-12 20:47:34 -0600

    Delete src directory

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser, getProfile } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/register', protect, registerUser); 
-router.get('/profile', protect, getProfile);
-
-module.exports = router;
